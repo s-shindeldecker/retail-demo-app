bff03c1ca116af6e38a3a28da4efc424d717d0c1
Ahmed <ahmedhqadri@gmail.com>
2024-09-30 19:22:19 -0700

    updated promo location

---
+++ components/Promotion.js
@@ -0,0 +1,10 @@
+
+const Promotion = () => (
+  <section className="py-2 px-4 bg-gray-100">
+    <p className="mt-2 justify-center text-center text-gray-700">
+      Get 20% off on all products. Use code: SAVE20
+    </p>
+  </section>
+);
+
+export default Promotion;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import shoes from '../data/shoes.json';
 import clothes from '../data/clothes.json';
 import FeatureFlag from '../components/FeatureFlag';
 import Head from 'next/head';
+import Promotion from '../components/Promotion';
 
 const Home = () => {
   const allProducts = [...shoes, ...clothes];
@@ -25,21 +26,17 @@ const Home = () => {
 
       {/* Main Content */}
       <main className="flex-1 p-4">
+        {/* Feature Flag Section */}
+        <FeatureFlag flagKey="new-promo-banner">
+          <Promotion />
+        </FeatureFlag>
+
         {/* Products Section */}
         <section>
-          <h2 className="text-2xl font-semibold mb-4">Products</h2>
           <ProductList products={allProducts} />
         </section>
 
-        {/* Feature Flag Section */}
-        <FeatureFlag flagKey="new-promo-banner">
-          <section className="py-8 px-4 bg-yellow-100">
-            <h2 className="text-2xl font-semibold">Special Promotion!</h2>
-            <p className="mt-2 text-gray-700">
-              Get 20% off on all products. Use code: SAVE20
-            </p>
-          </section>
-        </FeatureFlag>
+
       </main>
     </Layout>
   );
